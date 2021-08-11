bd4e20ccdb961252ecce76199de8a46939adc5df
SantiMerino <santiago.merino2004@gmail.com>
2021-08-11 12:03:17 -0600

    AHI TA REY

--- LabCRUD/Form1.cs
+++ LabCRUD/Form1.cs
@@ -276,5 +276,10 @@ namespace LabCRUD
                 e.Handled = true;
             }
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CargarGridDatos();
+        }
     }
 }
