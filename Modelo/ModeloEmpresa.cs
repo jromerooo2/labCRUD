9367c19a1633cc3a5e642acc3240684da8ce1ebd
SantiMerino <santiago.merino2004@gmail.com>
2021-08-11 09:28:13 -0600

    modelo empresa

--- Modelo/ModeloEmpresa.cs
+++ Modelo/ModeloEmpresa.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace Modelo
 {
     class ModeloEmpresa
     {
+        public static DataTable CargarTipoEmpresa()
+        {
+            DataTable data;
+            try
+            {
+                string instruccion = "SELECT * FROM tbempresa";
+                MySqlCommand cmdTipoEmpresa = new MySqlCommand(string.Format(instruccion), ModeloConexion.ObtenerConexion());
+                MySqlDataAdapter adp = new MySqlDataAdapter(cmdTipoEmpresa);
+                data = new DataTable();
+                adp.Fill(data);
+                return data;
+            }
+            catch (Exception)
+            {
+                return data = null;
+            }
+        }
+
+        public static DataTable CargarEstadoEmpresa()
+        {
+            DataTable data;
+            try
+            {
+                string instruccion = "SELECT * FROM tbestado";
+                MySqlCommand cmdEstadoEmpresa = new MySqlCommand(string.Format(instruccion), ModeloConexion.ObtenerConexion());
+                MySqlDataAdapter adp = new MySqlDataAdapter(cmdEstadoEmpresa);
+                data = new DataTable();
+                adp.Fill(data);
+                return data;
+            }
+            catch (Exception)
+            {
+                return data = null;
+            }
+        }
     }
 }
